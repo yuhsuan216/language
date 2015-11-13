514d38f55c7d6eb3bc3e619eaf7d413b608fb3f1
m516 <m516@m516>
2015-11-13 16:28:47 +0800

    CH8 9

---
+++ language/wiki/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from wiki.models import Category, Page
+
+
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(max_length=128, label='類別名稱', help_text='請輸入類別名稱')
+ 
+    class Meta:
+        model = Category
+        fields = ('name', )
+        
+        
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=128, label='頁面標題', help_text='請輸入頁面標題')
+    url = forms.URLField(max_length=128, label='頁面網址', help_text='請輸入頁面網址')
+    class Meta:
+        model = Page
+        exclude = ('category', 'views')
--- language/wiki/models.py
+++ language/wiki/models.py
@@ -7,7 +7,6 @@ class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
-    slug = models.SlugField()
     
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
@@ -18,12 +17,17 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
-    
+
 class Page(models.Model):
     category = models.ForeignKey(Category)
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
     
+    
+    def save(self, *args, **kwargs):
+        self.title = self.title.replace(' ', '-')
+        super(Page, self).save(*args, **kwargs)
+ 
     def __str__(self):
         return self.title
